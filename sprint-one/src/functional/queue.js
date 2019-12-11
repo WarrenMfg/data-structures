b5b07eaf72417ed499024f144644e3e3ec3ff815
WarrenMfg <warrenmfg@icloud.com>
2019-12-11 18:13:16 -0500

    completed functional instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var head = 0;
+  var tail = 0;
+
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[tail] = value;
+    tail++;
+    return someInstance.size();
   };
 
   someInstance.dequeue = function() {
+    if (tail > head) {
+      var value = storage[head];
+      delete storage[head];
+      head++;
+      return value;
+    } else {
+      return 0;
+    }
   };
 
   someInstance.size = function() {
+    return tail - head;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
+    return someInstance.size();
   };
 
   someInstance.pop = function() {
+    count >= 1 ? count-- : count = 0;
+    var value = storage.hasOwnProperty(count) ? storage[count] : 0;
+    delete storage[count];
+    return value;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
