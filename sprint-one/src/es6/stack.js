f7f956949d1da03735675375a6c7909d38f6e7ad
WarrenMfg <warrenmfg@icloud.com>
2019-12-12 10:54:37 -0500

    progress on ES6 instantiation

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,22 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
   }
 
+  push(value) {
+    this[this.count] = value;
+    this.count++;
+    return this.count;
+  }
+
+  pop() {
+    this.count >= 1 ? this.count-- : this.count = 0;
+    var value = this.hasOwnProperty(this.count) ? this[this.count] : 0;
+    delete this[this.count];
+    return value;
+  }
+
+  size() {
+    return this.count;
+  }
 }
