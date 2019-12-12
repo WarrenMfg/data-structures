ba987d71f16bf1916987410cc439e44cd07cc504
WarrenMfg <warrenmfg@icloud.com>
2019-12-12 18:35:38 -0500

    completed linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,47 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    let oldTail = list.tail;
+    let newNode = Node(value);
+    list.tail = newNode;
+
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      oldTail.next = newNode;
+    }
+
   };
 
   list.removeHead = function() {
+    var currentHead = list.head;
+    var nextHead = list.head.next;
+    list.head = nextHead;
+    return currentHead.value;
   };
 
   list.contains = function(target) {
+    // currentNode = head
+    var currentNode = list.head;
+    // while currentNode.next !== null
+    while (currentNode.next !== null) {
+      // if current.value === target
+      if (currentNode.value === target) {
+        //return true
+        return true;
+      } else {
+        // current = currentNode.next
+        currentNode = currentNode.next;
+      }
+    }
+
+    if (list.tail.value === target) {
+      return true;
+    } else {
+      return false;
+    }
+
+    return false;
   };
 
   return list;
@@ -26,4 +61,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+addToTail: Big O of 1
+removeHead: Big O of 1
+contiains: Big O of n
  */
