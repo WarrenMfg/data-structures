3179ab2cd401eb62f0aa1eba21b467744858d530
WarrenMfg <warrenmfg@icloud.com>
2019-12-11 22:23:18 -0500

    completed functional instantiation with shared methods

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -30,36 +30,3 @@ var queueMethods = {
 };
 
 var myQ = Queue();
-
-/*
-var Stack = function() {
-
-  var stack = {
-    storage: {},
-    count: 0
-  };
-
-  stack = _.extend(stack, stackMethods);
-
-  return stack;
-};
-
-var stackMethods = {
-  'push': function(value) {
-    this.storage[this.count] = value;
-    this.count++;
-    return this.count;
-  },
-  'pop': function() {
-    this.count >= 1 ? this.count-- : this.count = 0;
-    var value = this.storage[this.count];
-    delete this.storage[this.count];
-    return value;
-  },
-  'size': function() {
-    return this.count;
-  }
-};
-
-var myStack = Stack();
-*/
