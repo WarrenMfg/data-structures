fff50f6a4c769f6b663ba2009e24837d9c56bc61
WarrenMfg <warrenmfg@icloud.com>
2019-12-11 23:56:48 -0500

    completed prototypal instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.head = 0;
+  obj.tail = 0;
+  return obj;
 };
 
-var queueMethods = {};
-
+var queueMethods = {
+  enqueue: function(value) {
+    this[this.tail] = value;
+    this.tail++;
+    return this.size();
+  },
+  dequeue: function() {
+    if (this.tail > this.head) {
+      var value = this[this.head];
+      delete this[this.head];
+      this.head++;
+      return value;
+    } else {
+      return 0;
+    }
+  },
+  size: function() {
+    return this.tail - this.head;
+  }
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,22 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.count = 0;
+  return obj;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    this[this.count] = value;
+    this.count++;
+    return this.count;
+  },
+  pop: function() {
+    this.count >= 1 ? this.count-- : this.count = 0;
+    var value = this.hasOwnProperty(this.count) ? this[this.count] : 0;
+    delete this[this.count];
+    return value;
+  },
+  size: function() {
+    return this.count;
+  }
+};
