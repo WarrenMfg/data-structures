68ca6c5e28a90f5b6b433e132db47e6329daf03b
WarrenMfg <warrenmfg@icloud.com>
2019-12-12 19:51:48 -0500

    completed tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  newTree = _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +13,31 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  let node = Tree();
+  node['value'] = value;
+
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
+  var isPresent = false;
+  var traverse = function(node) {
+    if (node.value === target) {
+      isPresent = true;
+    }
+    for (let i = 0; i < node.children.length; i++) {
+      traverse(node.children[i]);
+    }
+  };
+  traverse(this);
+  return isPresent;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+Tree O(n) because of _.extend
+addChild O(n) because of let node = Tree() which uses _.extend
+contains O(n^2) because of for loop and recursion
  */
