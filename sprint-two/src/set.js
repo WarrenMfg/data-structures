d96c70645f4b4003330477f1f6846110a83c4b58
WarrenMfg <warrenmfg@icloud.com>
2019-12-14 14:02:36 -0500

    completed set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,67 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  //change this to object
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+//i/o
+//  input: string
+//  output: undefined
+//contraints:
+//  input must be a string
+//edge cases
+//  can only contain unique values
+//common patterns
+//  attribute assignment
+//plan
+//  create a new property in the set storage bject
+//    the key should be the argument passed to add
+//    the value shoule be true
+// elaborate
+//   to set's _storage object:
+//   assign new key value pair. Key is argument of .add, Pair is true
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+// i/o
+//   input: string
+//   output: boolean
+// constraints:
+//   input must be a string
+// edge cases
+//   item is not a string
+// common patterns
+//   property lookup
+// plan
+//   lookup property if it exists return true otherwise return false
+// elaborate:
+//   see above
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+// i/o
+//  input: string
+//  output: undefined
+// contraints:
+//   property with 'item' must exist to run
+// edge cases
+//   property does not exist
+//  common patterns:
+//  property deletion
+// plan
+//  if a property with a key equal to item argument exists,
+//     delete it
+// elaborate
+//   see above
+  if (this.contains(item)) {
+    delete this._storage[item];
+  }
 };
 
 /*
